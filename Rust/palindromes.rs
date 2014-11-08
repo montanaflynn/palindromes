cbccac5318151ac27f76695fc0f30e0b3fbf520e
Montana Flynn <montana@montanaflynn.me>
2014-11-07 17:27:48 -0800

    Optimize and update package name

--- Rust/palindromes.rs
+++ Rust/palindromes.rs
@@ -1,16 +1,16 @@
 use std::io::{File, BufferedReader};
 use std::iter::order;
-use std::ascii::StrAsciiExt;
+use std::ascii::AsciiExt;
 
 fn main() {
     let path = Path::new("/usr/share/dict/words");
     let mut file = BufferedReader::new(File::open(&path));
     for line in file.lines() {
         let line = line.unwrap();
-        let lowercase = line.as_slice().to_ascii_lower();
-        let word = lowercase.as_slice().trim_chars('\n');
-        let forwards = word.chars();
-        let backwards = word.chars().rev();
+        let word = line.as_slice().trim_chars('\n');
+        let lowercase = word.as_slice().to_ascii_lower();
+        let forwards = lowercase.chars();
+        let backwards = lowercase.chars().rev();
         if order::eq(forwards, backwards) {
             println!("{} is a palindrome", word);
         }
