e6745ef26a05a7408d642c0ab584bb27c469f487
Montana Flynn <montana@montanaflynn.me>
2014-11-07 17:28:12 -0800

    Formatting

--- Scala/palindromes.scala
+++ Scala/palindromes.scala
@@ -2,16 +2,17 @@ import scala.io.Source
 
 object Palindromes {
 
-  def isPalindrome(s: String): Boolean = {
+	def isPalindrome(s: String): Boolean = {
 		val p = s.toLowerCase
 		p == p.reverse
 	}
 
-  def main(args: Array[String]) {
+	def main(args: Array[String]) {
 		val filename = "/usr/share/dict/words"
 		for (word <- Source.fromFile(filename).getLines()) {
-			if (isPalindrome(word))
-			  println(String.format("%s is a palindrome", word))
+			if (isPalindrome(word)) {
+				println(String.format("%s is a palindrome", word))
+			}
 		}
-  }
+	}
 }
