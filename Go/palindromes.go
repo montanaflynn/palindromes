425971f5ca1c04b472efc88445803c4e3c731487
Boone Severson <severson@cray.com>
2015-10-16 11:45:31 -0500

    Shave a few thousands of a second off the Go implementation

--- Go/palindromes.go
+++ Go/palindromes.go
@@ -10,46 +10,36 @@ import (
 
 func main() {
 	file, err := os.Open("/usr/share/dict/words")
-	if err != nil { log.Fatal(err) }
+	if err != nil {
+		log.Fatal(err)
+	}
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		word := scanner.Text()
-		if isPalindrome(word) == true {
+		if isPalindrome(word) {
 			fmt.Printf("%s is a palindrome\n", word)
 		}
 	}
 }
 
 func isPalindrome(word string) bool {
-	var beg string
-	var end string
-	var wl int
+	lower := strings.ToLower(word)
+	last := len(lower) - 1
 
-	wl = len(word)
-	cut := wl / 2
-
-	beg = word[:cut]
-	beg = strings.ToLower(beg)
-
-	if wl % 2 == 0 {
-		end = word[cut:]
-	} else {
-		end = word[cut+1:]
+	for i := 0; i <= last; i++ {
+		// If pointing to the same letter (word with odd number of characters),
+		// or pointers have swapped halves, return true.
+		if i >= (last - i) {
+			return true
+		}
+		// If this check fails, return false instead of continuing.
+		if lower[i] != lower[last-i] {
+			return false
+		}
 	}
 
-	wl = len(end)
-	rev := make([]byte, wl)
-	wl--
-	for i := 0; i <= wl; i++ {
-		rev[wl-i] = end[i]
-	}
-	end = string(rev)
-
-	if beg == end {
-		return true
-	} else {
-		return false
-	}
+	// Should never get here, but return true to keep lint happy!
+	return true
 }
