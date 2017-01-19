96dfe769ca5df3c98b87193728c14f1d5fcbd7aa
Alex Ellis <alexellis2@gmail.com>
2017-01-19 08:38:30 +0000

    Make more Node-y
    * Read should be asynchronous
    * Eliminate toString() by specifying the encoding as UTF8
    * Use up to date ES2015 style via "use strict"

--- JavaScript/palindromes.js
+++ JavaScript/palindromes.js
@@ -1,10 +1,12 @@
-var fs = require('fs')
+"use strict"
+const fs = require('fs')
  
-var words = fs.readFileSync('/usr/share/dict/words').toString().split("\n")
-
-for (var i = 0, len = words.length; i < len; i++) {
-  if (words[i].length === 0) continue
-  var forwards = words[i].toLowerCase()
-  var backwards = forwards.split("").reverse().join("")
-  if (forwards === backwards) console.log(words[i] + " is a palindrome")
-}
+fs.readFile("/usr/share/dict/words", "utf8", (err, text) => {
+    var words = text.split("\n");
+    for(var i = 0, len = words.length; i < len; i++) {
+      if (words[i].length === 0) continue
+      var forwards = words[i].toLowerCase()
+      var backwards = forwards.split("").reverse().join("")
+      if (forwards === backwards) console.log(words[i] + " is a palindrome")
+    }
+});
