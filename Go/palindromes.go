4a58de7220483a7a68fca212e8950a87a2ba07d3
Sarath S Pillai <sarath.sp06@gmail.com>
2018-04-17 21:43:03 +0530

    Update palindromes.go
    Update to Go palindorme with fewer lines of code and lesser comparisons (for non palindrom cases)

--- Go/palindromes.go
+++ Go/palindromes.go
@@ -5,14 +5,12 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strings"
 )
 
 func main() {
 	file, err := os.Open("/usr/share/dict/words")
 	if err != nil { log.Fatal(err) }
 	defer file.Close()
-
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		word := scanner.Text()
@@ -23,33 +21,11 @@ func main() {
 }
 
 func isPalindrome(word string) bool {
-	var beg string
-	var end string
 	var wl int
-
 	wl = len(word)
-	cut := wl / 2
-
-	beg = word[:cut]
-	beg = strings.ToLower(beg)
-
-	if wl % 2 == 0 {
-		end = word[cut:]
-	} else {
-		end = word[cut+1:]
+        mid := int(wl/2)
+	for i:=0;i<=mid;i++ {
+		if word[i] != word[wl-1-i]{return false}
 	}
-
-	wl = len(end)
-	rev := make([]byte, wl)
-	wl--
-	for i := 0; i <= wl; i++ {
-		rev[wl-i] = end[i]
-	}
-	end = string(rev)
-
-	if beg == end {
-		return true
-	} else {
-		return false
-	}
+	return true
 }
